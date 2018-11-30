1ff9784078ad150fc9f5fb5ef64afe12ef07e31f
shanpramuditha <pramuditha.14@cse.mrt.ac.lk>
2018-11-30 09:23:41 +0530

    Add logging to file

--- Distributed_Systems/client.py
+++ Distributed_Systems/client.py
@@ -13,6 +13,8 @@ from itertools import groupby
 import shutil
 import math
 import time
+import hashlib
+
 
 
 parser = argparse.ArgumentParser(description='Client for Bootstrap Server')
@@ -26,7 +28,6 @@ parser.add_argument('-connections', dest='connections', type=int, action='store'
                     default=2, help='No of Connections')
 
 args = parser.parse_args()
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 FILES_DIRECTORY = 'files'
 
@@ -38,6 +39,9 @@ bs_port = int(args.bs.split(':')[1])
 username = args.username
 connectionsCount = args.connections
 
+logging.basicConfig(filename=username+".log",format='%(levelname)s:%(message)s', level=logging.INFO)
+
+
 peerTable = []
 peers = []
 clientFiles = []
@@ -47,11 +51,11 @@ stockWords = ['of', 'for', 'the', 'up', 'a', 'and']
 discover_nodes = []
 
 isActive = False
-# logging.info("Client: %s:%i" % (client_ip,client_port))
+logging.info("Client: %s:%i" % (client_ip,client_port))
 
 def sendUDP(ip, port, message):
-    # logging.info("recipient UDP client: %s:%i" % (ip,port))
-    # logging.info("message: %s to %s" % (message, port))
+    logging.info("recipient UDP client: %s:%i" % (ip,port))
+    logging.info("message: %s to %s" % (message, port))
     sockUDP = socket.socket(socket.AF_INET,  # Internet
                             socket.SOCK_DGRAM)  # UDP
     sockUDP.sendto(message.encode('utf-8'), (ip, int(port)))
@@ -64,8 +68,8 @@ def inputParser(input):
         if peer not in peers:
             peers.append(peer)
             message = "JOINOK 0"
-            # logging.info(
-                # "JOIN Request Accepted from %s:%d to %s:%d" % (peer['ip'], int(peer['port']), client_ip, client_port))
+            logging.info(
+                "JOIN Request Accepted from %s:%d to %s:%d" % (peer['ip'], int(peer['port']), client_ip, client_port))
         else:
             message = "JOINOK 9999"
             logging.warning(
@@ -100,7 +104,7 @@ def inputParser(input):
         discover_nodes.append(peer)
         message = "ADDOK 0"
 
-        #     logging.warning("ADD Request Rejected from %s:%s to %s:%d" % (peer['ip'], peer['port'], client_ip, client_port))
+        logging.warning("ADD Request Rejected from %s:%s to %s:%d" % (peer['ip'], peer['port'], client_ip, client_port))
 
         message = "%04d %s" % (len(message) + 5, message)
         sendUDP(peer['ip'], int(peer['port']), message)
@@ -130,7 +134,7 @@ def inputParser(input):
                         message = "SER %s %s %s %d" % (ip, port, file_name, hops)
                         message = "%04d %s" % (len(message) + 5, message)
                         sendUDP(neighbor['ip'], int(neighbor['port']), message)
-                    # logging.info("SER Request to %s:%s with %s hops" % (neighbor['ip'], neighbor['port'], hops))
+                    logging.info("SER Request to %s:%s with %s hops" % (neighbor['ip'], neighbor['port'], hops))
             else:
                 message = "SEROK %d %s %d %d" % (0, client_ip, client_port, hops)
                 message = "%04d %s" % (len(message) + 5, message)
@@ -190,10 +194,10 @@ class UDPServer(threading.Thread):
         self.daemon = True
 
     def run(self):
-        # logging.info("USD server started")
+        logging.info("USD server started")
         while True:
             data, addr = self.sock.recvfrom(10240)  # buffer size is 1024 bytes
-            # logging.info("received message: %s" % data)
+            logging.info("received message: %s" % data)
             inputParser(data[5:])
 
 class TCPServer(threading.Thread):
@@ -212,9 +216,10 @@ class TCPServer(threading.Thread):
         while True:
             try:
                 conn, addr = self.sock.accept()
+                logging.info("TCP server started")
             except Exception as e:
                 logging.warning(e)
-        # logging.info("TCP server started")
+
 
             data = conn.recv(1024)
             if not data:
@@ -225,6 +230,10 @@ class TCPServer(threading.Thread):
             f = open(abs_path, 'rb')
 
             l = f.read()
+            h = hashlib.sha256()
+            h.update(l)
+            h = h.hexdigest()
+            l = h.encode('utf-8')+l
             conn.send(l)
             conn.close()
 
@@ -235,6 +244,7 @@ class Gossip(threading.Thread):
 
     def run(self):
         while(True):
+            time.sleep(20)
             Discover(self.hops)
             time.sleep(20)
 
@@ -249,7 +259,7 @@ def sendTCP(ip, port, message):
     soc.send(message.encode('utf-8'))
     dataReceived = soc.recv(1024*1024*12).decode('utf-8')
     soc.close()
-    # logging.info("Data received: %s" % dataReceived)
+    logging.info("Data received: %s" % dataReceived)
     return dataReceived
 
 def joinPeers(ip, port, peersIndex):
@@ -258,8 +268,8 @@ def joinPeers(ip, port, peersIndex):
         message = "%04d %s" % (len(message) + 5, message)
         sendUDP(peerTable[node]['ip'], int(peerTable[node]['port']), message)
         peers.append(peerTable[node])
-        # logging.info(
-        #     "JOIN Request Sent from %s:%d to %s:%d" % (ip, port, peerTable[node]['ip'], int(peerTable[node]['port'])))
+        logging.info(
+            "JOIN Request Sent from %s:%d to %s:%d" % (ip, port, peerTable[node]['ip'], int(peerTable[node]['port'])))
 
 
 def registerClient(ip, port, bs_ip, bs_port, username):
@@ -268,12 +278,12 @@ def registerClient(ip, port, bs_ip, bs_port, username):
     response = sendTCP(bs_ip, bs_port, message)
     decodedResponse = response.split()
     peers = int(decodedResponse[2])
-    # logging.warning(response)
+    logging.warning(response)
 
     if(peers == 0):
-        # logging.warning("no nodes in the system")
-        # logging.info("Successfully Registered !!")
-        # logging.info("Starting UDP Server on %s:%d" % (ip, port))
+        logging.warning("no nodes in the system")
+        logging.info("Successfully Registered !!")
+        logging.info("Starting UDP Server on %s:%d" % (ip, port))
         udp = UDPServer(ip, port)
         udp.start()
         tcp = TCPServer(ip, str(port) + "0")
@@ -295,14 +305,14 @@ def registerClient(ip, port, bs_ip, bs_port, username):
         logging.warning("failed can't register BS full")
         return False
     else:
-        # logging.info("No of peers connected to BS: %i" % peers)
+        logging.info("No of peers connected to BS: %i" % peers)
         for i in range(0, peers * 3, 3):
             peerTable.append({'ip': decodedResponse[i + 3], 'port':decodedResponse[i + 4]})
-        # logging.debug(peerTable)
+        logging.debug(peerTable)
 
-        # logging.info("Successfully Registered !!")
+        logging.info("Successfully Registered !!")
 
-        # logging.info("Starting UDP Server on %s:%d" % (ip, port))
+        logging.info("Starting UDP Server on %s:%d" % (ip, port))
 
         udp = UDPServer(ip, port)
         udp.start()
@@ -340,7 +350,7 @@ def Discover(hops):
             message = "%04d %s" % (len(message) + 5, message)
             sendUDP(neighbour['ip'],int(neighbour['port']),message)
 
-    print("$ Discovering ...")
+    logging.info("Discovering ...")
     time.sleep(2)
     for neighbour in peers:
         if not checkStatus(neighbour,discover_nodes):
@@ -357,7 +367,7 @@ def Discover(hops):
     if not checkStatus(self_node,peerTable):
         peerTable.append(self_node)
 
-    print("$ Routing Tables Updated !\n$ ")
+    logging.info("Routing Tables Updated !")
 
 def checkStatus(peer, list):
     for node in list:
@@ -374,10 +384,10 @@ def Unregister(ip, port):
     if code == 0:
         isActive = False
         return True
-        # logging.info("Leaving Successfull!!!")
+        logging.info("Leaving Successfull!!!")
     else:
         return False
-        # logging.info("Error while adding new node to routing table!!!")
+        logging.info("Error while adding new node to routing table!!!")
 
 def searchFile(file_name):
     serch_result = []
@@ -411,15 +421,26 @@ def search(file_name):
 
             message = "%04d %s" % (len(message) + 5, message)
             sendUDP(neighbor['ip'], int(neighbor['port']), message)
-            # logging.info("SER Request to %s:%s with %s hops" % (neighbor['ip'], neighbor['port'], 5))
+            logging.info("SER Request to %s:%s with %s hops" % (neighbor['ip'], neighbor['port'], 5))
 
 def downloadFile(file):
     response = sendTCP(file['ip'], int(file['port']), file['file'])
+    response = response.encode('utf-8')
+    hash = response[:64]
+    response=response[64:]
+    h = hashlib.sha256()
+    h.update(response)
+    h=h.hexdigest()
+
+    # response = response.decode('utf')
+    print("$ Received Hash : "+hash.decode("utf-8")+" , Calculated Hash : "+h)
     dest_path = 'node_files/' + username + "_files"
     abs_path = os.path.abspath(dest_path + '/received_files/'+file['file'])
     with open(abs_path, 'wb') as f:
-        f.write(response.encode('utf-8'))
+        # f.write(response.encode('utf-8'))
+        f.write(response)
     f.close()
+
     print('$ Successfully downloaded the file')
 
 def commandParser(command):
